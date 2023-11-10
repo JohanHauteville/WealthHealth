219c96798330e405fe79b1c9239cdd59fc0b4dff
JohanHauteville <johan.hauteville@gmail.com>
2023-11-10 16:10:33 +0100

    init DatePicker component

---
+++ src/components/DatePicker/index.jsx
@@ -0,0 +1,12 @@
+import "./styles.scss";
+
+function DatePicker() {
+  return (
+    <div>
+      <p>coucou</p>
+      <p>ça va ?</p>
+    </div>
+  );
+}
+
+export default DatePicker;
--- src/pages/EmployeesList/index.jsx
+++ src/pages/EmployeesList/index.jsx
@@ -1,9 +1,11 @@
 import "./styles.scss";
+import DatePicker from "../../components/DatePicker";
 
 function EmployeesList() {
   return (
     <main className="employees-page">
       <h2>Current Employees</h2>
+      <DatePicker />
     </main>
   );
 }
